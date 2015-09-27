9340c814cd939b0cf425dc0400964bfa92eff053
dukegod <dukehui1991@gmail.com>
2015-09-28 05:30:18 +0800

    spider
    爬虫小程序，慢慢研究

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var cheerio = require('cheerio');
+
+app.get('/', function (req, res) {
+  request('http://www.qshealthcare.com/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(body); 
+      // $ = cheerio.load($('section'));
+      // console.log($);
+      // $.html(); 
+      $ = cheerio.load(body);
+      var p = $('p.qs-identity').text();
+
+      var section = $('section');
+
+      console.log(p);
+      console.log(section.html()); 
+      res.send(p);
+      res.send(section);
+    }
+  })
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at', host, port);
+});
