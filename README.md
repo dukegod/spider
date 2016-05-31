38feb73d7f368ac80c6a633e38a1383707a7ff58
liuhui <dukehui1991@gmail.com>
2016-05-31 14:48:18 +0800

    add eventproxy instance use async for multi acesser

--- README.md
+++ README.md
@@ -9,6 +9,15 @@ supervisor app.js
 ```
 自动刷新
 
-
 注意查看robot.txt
+
+使用eventproxy控制并发
+[eventproxy](https://github.com/JacksonTian/eventproxy)
+
+或者使用async
+[async](https://github.com/caolan/async)
+
+当你需要去多个源(一般是小于 10 个)汇总数据的时候，用 eventproxy 方便；当你需要用到队列，需要控制并发数，或者你喜欢函数式编程思维时，使用 async。大部分场景是前者，所以我个人大部分时间是用 eventproxy 的。
+
+
 
--- app.js
+++ app.js
@@ -3,25 +3,89 @@ var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
 
+var eventproxy = require('eventproxy');
+var url = require('url');
+
+// app.get('/', function (req, res) {
+//   request('https://cnodejs.org/', function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       $ = cheerio.load(body);
+//       // console.log($);
+//       var items = [];
+//       $('#topic_list .topic_title').each(function (idx, element) {
+//         var $element = $(element);
+//         items.push({
+//           title: $element.attr('title'),
+//           href: $element.attr('href')
+//         });
+//       });
+//       res.send(items);
+//     }
+//   })
+// });
+
+/*
+*
+*  整合eventproxy完成并发处理
+*/
 app.get('/', function (req, res) {
-  request('http://www.qshealthcare.com/', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-
-      $ = cheerio.load(body);
-      var p = $('p.qs-identity').text();
-
-      var section = $('section');
-
-      console.log(p);
-      console.log(section.html()); 
-      res.send(p);
-      res.send(section);
+  var cnodeUrl = 'https://cnodejs.org/';
+  request(cnodeUrl,function (err, res, body) {
+    // console.log('----body',body);
+    if (err) {
+      return console.error(err);
     }
-  })
+    var topicUrls = [];
+    var $ = cheerio.load(body);
+    // console.log($);
+    // 获取首页所有的链接
+    $('#topic_list .topic_title').each(function (idx, element) {
+      var $element = $(element);
+      // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
+      // 我们用 url.resolve 来自动推断出完整 url，变成
+      // https://cnodejs.org/topic/542acd7d5d28233425538b04 的形式
+      // 具体请看 http://nodejs.org/api/url.html#url_url_resolve_from_to 的示例
+      var href = url.resolve(cnodeUrl, $element.attr('href'));
+      topicUrls.push(href);
+    });
+
+    var ep = new eventproxy();
+
+    ep.after('topic_html', topicUrls.length, function (topics) {
+      topics = topics.map(function (topicPair) {
+        var topicUrl = topicPair[0];
+        var topicHtml = topicPair[1];
+        console.log(topicHtml);
+        var $ = cheerio.load(topicHtml);
+        return ({
+          title: $('.topic_full_title').text().trim(),
+          href: topicUrl,
+          comment1: $('.reply_content').eq(0).text().trim(),
+        });
+      });
+
+      console.log('final:');
+      console.log(topics);
+    });
+
+    topicUrls.forEach(function (topicUrl) {
+      request
+        .get(topicUrl,function(){
+          console.log('fetch ' + topicUrl + ' successful');
+          ep.emit('topic_html', [topicUrl, body]);
+        })
+    });
+
+    console.log(topicUrls);
+  });
+  res.send('ok');
 });
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at', host, port);
 });
+
+module.exports = app
